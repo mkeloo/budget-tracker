b6458f4ac4c982ba0344e495b016e589d2f6f9fe
mkeloo <mkeloo@ufl.edu>
2024-06-29 11:22:02 -0400

    project setup and database setup with prisma.

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import RootProviders from '@/components/providers/RootProviders';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Budget Tracker App',
+  description: 'Get your finances in order with Budget Tracker App.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className="dark" style={{ colorScheme: 'dark' }}>
+        <body className={inter.className}>
+          <RootProviders>{children}</RootProviders>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
